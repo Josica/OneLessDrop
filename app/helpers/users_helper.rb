11a96edd3049116865cc037a99fdb5b4f39a049c
Josica <josica.ganesamurthy@gmail.com>
2017-06-25 01:55:13 -0400

    add users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def current_user
+    @current_user ||= User.find_by(id: sessions[:user_id])
+  end
+
+  def login?
+    !!current_user
+  end
+
+  def require_user
+    redirect_to login_path unless current_user
+  end
+
 end
