f2ba99cabaa3beaa99b369d66ff6b3615b1bea15
Josica <josica.ganesamurthy@gmail.com>
2017-06-24 21:33:00 -0400

    add changes to tables

--- db/migrate/20170625011254_create_averages.rb
+++ db/migrate/20170625011254_create_averages.rb
@@ -1,7 +1,16 @@
 class CreateAverages < ActiveRecord::Migration
   def change
     create_table :averages do |t|
-
+      t.integer :bath, {null: false}
+      t.integer :shower, {null: false}
+      t.integer :toilet, {null: false}
+      t.integer :dishes, {null: false}
+      t.integer :toothbrush, {null: false}
+      t.integer :car, {null: false}
+      t.integer :garden, {null: false}
+      t.integer :laundry, {null: false}
+      t.integer :cooking, {null: false}
+      
       t.timestamps null: false
     end
   end
